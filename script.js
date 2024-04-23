7e9c93857092c930ccc01ba85513aa11a324fa3c
ZubitZhu <125566541+ZubitZhu@users.noreply.github.com>
2024-04-23 09:00:54 +0200

    misc

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 function generaNpc() {
     //var stringaDestinazione = document.getElementById("risultatoNPC");
     //stringaDestinazione.innerHTML = "Io sono il tuo nuovo NPC!"
-    alert("Livello Chaos: " + getChaosLevel());
+    alert("Livello Chaos: " + getChaosLevel);
 }
 
-function getChaosLevel(){
+function getChaosLevel() {
     var chaosLevel = document.getElementById("livelloCaos").value;
-    if(chaosLevel <= 0 || chaosLevel > 9){
+    if (chaosLevel <= 0 || chaosLevel > 9) {
         alert("Livello chaos errato. Per favore, imposta un valore da 1 a 9.");
         return 1;
     }
