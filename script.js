985d6b316c34bd2eecd257da39f963e2bb8e550a
ZubitZhu <125566541+ZubitZhu@users.noreply.github.com>
2024-04-23 08:46:04 +0200

    misc

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-function generaNpc(){
+function generaNpc() {
     var stringaDestinazione = document.getElementById("risultatoNPC");
     stringaDestinazione.innerHTML = "Io sono il tuo nuovo NPC!"
 }
