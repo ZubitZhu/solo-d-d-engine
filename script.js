4f231c3585d9e822254140cf0d41cf8efa371285
ZubitZhu <125566541+ZubitZhu@users.noreply.github.com>
2024-04-23 09:06:19 +0200

    Test funzione d100

--- script.js
+++ script.js
@@ -2,14 +2,19 @@ function generaNpc() {
     //var stringaDestinazione = document.getElementById("risultatoNPC");
     //stringaDestinazione.innerHTML = "Io sono il tuo nuovo NPC!"
     alert("Livello Chaos: " + getChaosLevel);
+    alert("Tiro d100: " + dicePercent);
 }
 
 function getChaosLevel() {
     var chaosLevel = document.getElementById("livelloCaos").value;
     if (chaosLevel <= 0 || chaosLevel > 9) {
         alert("Livello chaos errato. Per favore, imposta un valore da 1 a 9.");
-        return 1;
+        return;
     }
     return chaosLevel;
+}
+
+function dicePercent() {
+    return Math.floor(Math.random() * 100) + 1;
 }
 
